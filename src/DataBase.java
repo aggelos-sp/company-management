5cc9f0b6ad2ebf69d3de1f7a5be7c90eed066eb5
aggelos-sp <45153901+aggelos-sp@users.noreply.github.com>
2020-01-13 21:31:22 +0200

    Added Libraries and made simple gui for initialization

---
+++ src/DataBase.java
@@ -0,0 +1,57 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBase {
+    private Connection con;
+    private String url, databaseName, username, password;
+    private int port;
+    DataBase(String url, String databaseName, int port, String username, String password){
+        this.url = url;
+        this.databaseName = databaseName;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url + ":" + port + "/" +
+                    databaseName + "?characterEncoding=UTF-8", username, password);
+        }catch (SQLException se){
+            System.out.println("Exception trying to establish connection with the database!!");
+            se.printStackTrace();
+        }catch (ClassNotFoundException ce){
+            System.out.println("Exception setting driver!!");
+            ce.printStackTrace();
+        }
+    }
+    public void db_init(){
+        try {
+            String sql = "CREATE TABLE REGISTRATION " +
+                    "(id INTEGER not NULL, " +
+                    " first VARCHAR(255), " +
+                    " last VARCHAR(255), " +
+                    " age INTEGER, " +
+                    " PRIMARY KEY ( id ))";
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(sql);
+        }catch (SQLException se){
+            System.out.println("Exception during initialization!!");
+            while (se != null){
+                System.out.println("Message: " + se.getMessage());
+                System.out.println("SQLState: " + se.getSQLState());
+                System.out.println("ErrorCode: " + se.getErrorCode());
+                se = se.getNextException();
+            }
+        }
+    }
+
+    public void db_close(){
+        try {
+            con.close();
+        }catch (SQLException e){
+            System.out.println("Exception trying to close connection!!");
+            e.printStackTrace();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,41 @@
 import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Gui");
-        frame.setContentPane(new Gui().getMyPanel());
+        //Start database
+        DataBase db = new DataBase("jdbc:mysql://localhost", "cs360", 3306,
+                "root","");
+
+        JFrame frame=new JFrame("DataBase");
+        final JTextField input_text = new JTextField();
+        input_text.setBounds(50,50, 150,20);
+        //add initialization button
+        JButton button_initialize = new JButton("Init");
+        button_initialize.setBounds(50,100,95,30);
+        button_initialize.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                db.db_init();
+            }
+        });
+        //add termination button
+        JButton button_close = new JButton("Close Connection");
+        button_close.setBounds(50, 150, 100,20);
+        button_close.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                db.db_close();
+            }
+        });
+
+
+        frame.add(button_initialize);
+        frame.add(button_close);
+        frame.add(input_text);
+        frame.setSize(400,400);
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
     }
 }
