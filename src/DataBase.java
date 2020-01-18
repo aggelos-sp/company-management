feace75fb1a1fb7290d85a6788da18dafda54608
aggelos-sp <45153901+aggelos-sp@users.noreply.github.com>
2020-01-18 21:58:42 +0200

    added the proper tables

--- src/DataBase.java
+++ src/DataBase.java
@@ -37,13 +37,74 @@ public class DataBase {
     }
     public void db_init(){
         try {
-            String sql = "CREATE TABLE REGISTRATION " +
+            Statement stmt = con.createStatement();
+            String sql = "CREATE TABLE permanent_staff " +
                     "(id INTEGER not NULL, " +
                     " first VARCHAR(255), " +
                     " last VARCHAR(255), " +
                     " age INTEGER, " +
+                    " iban VARCHAR(255),"+
+                    " address VARCHAR(255),"+
+                    " marital_status BOOL,"+
+                    " family_allowance INTEGER,"+
+                    " work_years INTEGER,"+
+                    " bank_name VARCHAR(255),"+
+                    " children_num INTEGER,"+
                     " PRIMARY KEY ( id ))";
-            Statement stmt = con.createStatement();
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE permanent_phones"+
+                " (id INTEGER not NULL,"+
+                " phone INTEGER,"+
+                " PRIMARY KEY (id))";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE contract_staff " +
+                    "(id INTEGER not NULL, " +
+                    " first VARCHAR(255), " +
+                    " last VARCHAR(255), " +
+                    " age INTEGER, " +
+                    " iban VARCHAR(255),"+
+                    " address VARCHAR(255),"+
+                    " marital_status BOOL,"+
+                    " family_allowance INTEGER,"+
+                    " work_years INTEGER,"+
+                    " bank_name VARCHAR(255),"+
+                    " children_num INTEGER,"+
+                    " start_date VARCHAR(255),"+
+                    " end_date VARCHAR(255),"+
+                    " PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE contract_phones"+
+                    " (id INTEGER not NULL,"+
+                    " phone INTEGER,"+
+                    " PRIMARY KEY (id))";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE children"+
+                "( name VARCHAR(255),"+
+                " age INTEGER,"+
+                " id INTEGER not NULL,"+
+                " PRIMARY KEY (id))";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE  department"+
+                " (dep_name VARCHAR (255) not NULL,"+
+                " hire_date VARCHAR(255),"+
+                " id INTEGER ,"+
+                " PRIMARY KEY(dep_name)) ";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE allowance_permanent"+
+                " (id INTEGER not NULL,"+
+                " research_allowance INTEGER,"+
+                " PRIMARY KEY (id))";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE allowance_contract"+
+                    " (id INTEGER not NULL,"+
+                    " library_allowance INTEGER,"+
+                    " PRIMARY KEY (id))";
             stmt.executeUpdate(sql);
         }catch (SQLException se){
             sql_exception_handle(se);
@@ -52,8 +113,32 @@ public class DataBase {
 
     public void db_close(){
         try {
-            String sql = "DROP TABLE REGISTRATION ";
+            String sql = "DROP TABLE permanent_staff ";
             Statement stmt = con.createStatement();
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE contract_staff ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE contract_phones ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE permanent_phones ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE children ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE department ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE allowance_permanent ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE allowance_contract ";
+            stmt.executeUpdate(sql);
+
+            sql = "DROP TABLE contract ";
             stmt.executeUpdate(sql);
             con.close();
         }catch (SQLException se){
